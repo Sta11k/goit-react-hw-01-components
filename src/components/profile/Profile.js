265fbc364d265171bc6c1148d16a2367687dcabf
Sta11k <alexandiel1984@gmail.com>
2021-10-02 18:19:41 +0300

    start part-1

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -6,30 +6,30 @@ console.log(user);
 function Profile() {
   return (
     <div className="Profile">
-      <div class="profile">
-        <div class="description">
+      <div className="profile">
+        <div className="description">
           <img
             src="https://www.flaticon.com/svg/static/icons/svg/3135/3135715.svg"
             alt="Аватар пользователя"
-            class="avatar"
+            className="avatar"
           />
-          <p class="name">Petra Marica</p>
-          <p class="tag">@pmarica</p>
-          <p class="location">Salvador, Brasil</p>
+          <p className="name">Petra Marica</p>
+          <p className="tag">@pmarica</p>
+          <p className="location">Salvador, Brasil</p>
         </div>
 
-        <ul class="stats">
+        <ul className="stats">
           <li>
-            <span class="label">Followers</span>
-            <span class="quantity">1000</span>
+            <span className="label">Followers</span>
+            <span className="quantity">1000</span>
           </li>
           <li>
-            <span class="label">Views</span>
-            <span class="quantity">2000</span>
+            <span className="label">Views</span>
+            <span className="quantity">2000</span>
           </li>
           <li>
-            <span class="label">Likes</span>
-            <span class="quantity">3000</span>
+            <span className="label">Likes</span>
+            <span className="quantity">3000</span>
           </li>
         </ul>
       </div>
@@ -37,14 +37,12 @@ function Profile() {
   );
 }
 
-{
-  /* <Profile
+/* <Profile
   name={user.name}
   tag={user.tag}
   location={user.location}
   avatar={user.avatar}
   stats={user.stats}
 />; */
-}
 
 export default Profile;
