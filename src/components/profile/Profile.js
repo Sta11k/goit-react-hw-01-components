25b54fc005e56247af2389d13ddc2c1c142364ef
Sta11k <alexandiel1984@gmail.com>
2021-10-03 13:58:53 +0300

    add part2

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -3,46 +3,47 @@ import user from './user.json';
 
 console.log(styles);
 console.log(user);
-function Profile() {
+
+//  <Profile
+//   name={user.name}
+//   tag={user.tag}
+//   location={user.location}
+//   avatar={user.avatar}
+//   stats={user.stats}
+// />;
+
+function Profile(props) {
   return (
     <div className="Profile">
       <div className="profile">
         <div className="description">
-          {/* <img
-            src="https://www.flaticon.com/svg/static/icons/svg/3135/3135715.svg"
+          <img
+            src={props.avatar}
             alt="Аватар пользователя"
             className="avatar"
-          /> */}
-          <p className="name">Petra Marica</p>
-          <p className="tag">@pmarica</p>
-          <p className="location">Salvador, Brasil</p>
+          />
+          <p className="name">{props.name}</p>
+          <p className="tag">@{props.tag}</p>
+          <p className="location">{props.location}</p>
         </div>
 
         <ul className="stats">
           <li>
-            <span className="label">Followers</span>
-            <span className="quantity">1000</span>
+            <span className="label">Followers </span>
+            <span className="quantity">{props.stats.followers}</span>
           </li>
           <li>
-            <span className="label">Views</span>
-            <span className="quantity">2000</span>
+            <span className="label">Views </span>
+            <span className="quantity">{props.stats.views}</span>
           </li>
           <li>
-            <span className="label">Likes</span>
-            <span className="quantity">3000</span>
+            <span className="label">Likes </span>
+            <span className="quantity">{props.stats.likes}</span>
           </li>
         </ul>
       </div>
     </div>
   );
 }
-
-/* <Profile
-  name={user.name}
-  tag={user.tag}
-  location={user.location}
-  avatar={user.avatar}
-  stats={user.stats}
-/>; */
 
 export default Profile;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,19 @@ import ReactDOM from 'react-dom';
 import 'modern-normalize/modern-normalize.css';
 import './index.css';
 import Profile from '../src/components/profile/Profile';
+import user from '../src/components/profile/user.json';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    <Profile />
+    <Profile
+      name={user.name}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats}
+    />
   </React.StrictMode>,
   document.getElementById('root'),
 );
