1fe693fe49a46d71d149c5aa7fcfe7543dc37a79
Sta11k <Alexandiel1984@gmail.com>
2021-10-08 14:49:10 +0300

    css color

--- src/components/transactionHistory/TransactionHistory.js
+++ src/components/transactionHistory/TransactionHistory.js
@@ -2,8 +2,6 @@ import './TransactionHistory.css';
 import transactions from './transactions.json';
 
 export default function TransactionHistory(props) {
-  //  const { id, label, percentage } = props;
-  //   const { id, type, amount, currency } = props;
   return (
     <table className="transaction-history">
       <thead className="thead-history">
@@ -16,7 +14,7 @@ export default function TransactionHistory(props) {
 
       <tbody className="tbodyh-history">
         {transactions.map(el => (
-          <tr key={el.id}>
+          <tr key={el.id} className=" switchColor">
             <td>{el.type}</td>
             <td>{el.amount}</td>
             <td>{el.currency}</td>
@@ -24,26 +22,5 @@ export default function TransactionHistory(props) {
         ))}
       </tbody>
     </table>
-    // <div className="TransactionHistory">
-    //   <ul className="friend-list">
-    //     {friends.map(el => (
-    // <tr>
-    //   <td>Withdrawal</td>
-    //   <td>85</td>
-    //   <td>USD</td>
-    // </tr>
-    //       <li className="friend-item" key={el.id}>
-    //         {el.isOnline === true ? (
-    //           <span className="statusOn">&#x263A;</span>
-    //         ) : (
-    //           <span className="statusOf">&#x263A;</span>
-    //         )}
-
-    //         <img className="avatar" src={el.avatar} alt="avatar" width="48" />
-    //         <p className="name">{el.name}</p>
-    //       </li>
-    //     ))}
-    //   </ul>
-    // </div>
   );
 }
